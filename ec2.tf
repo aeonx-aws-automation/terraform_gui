012661cf5688a341c492b297184e4c71c3b4bb4a
aeonx-aws-automation <aws.automation@aeonx.digital>
2024-06-04 15:00:30 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,8 @@
 provider "aws" {
 
     region = "ap-south-1"
-    access_key = ""  
-    secret_key = ""
+    access_key = var.access_key  
+    secret_key = var.secret_key
 }
 
 resource "aws_instance" "demo-instance-1" {
