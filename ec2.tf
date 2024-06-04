249090bf2292b0e41c0829716d3a8e1a9ac327c0
aeonx-aws-automation <aws.automation@aeonx.digital>
2024-06-04 15:23:57 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -7,7 +7,7 @@ provider "aws" {
 
 resource "aws_instance" "demo-instance-1" {
 
-    ami = "ami-0ae12517fe595ce17"
+    ami = "ami-0f58b397bc5c1f2e8"
     instance_type = "t2.micro"
      tags = {
         Name = "demo-instance"
